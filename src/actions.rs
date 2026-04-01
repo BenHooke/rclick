055631856c9eae496f304670a0a9ea3df2b11ade
Ben Hooke <BenHooke73@live.ca>
2026-04-01 18:00:52 -0400

    Build project TODO skeleton

---
+++ src/actions.rs
@@ -0,0 +1,106 @@
+use std::env;
+use std::path::PathBuf;
+use std::process::Command;
+
+use dualoguer::{Confirm, Input, Select, theme::ColorfulTheme};
+
+#[derive(Debug)]
+pub enum GeneralAction {
+    NewFile,
+    NewFolder,
+    Open,
+    Search,
+    CopyPath,
+    Histroty,
+}
+
+#[derive(Debug)]
+pub enum FileAction {
+    Rename,
+    Copy,
+    Move,
+    Open,
+    View,
+    Permissions,
+    Compress,
+    Delete,
+}
+
+pub fn run_general() {
+    todo!()
+}
+
+pub fn run_file() {
+    todo!()
+}
+
+fn action_new_file() {
+    todo!()
+}
+
+fn action_new_dir() {
+    todo!()
+}
+
+fn action_open_picker() {
+    todo!()
+}
+
+fn action_search() {
+    todo!()
+}
+
+fn action_copy_path() {
+    todo!()
+}
+
+fn action_history() {
+    todo!()
+}
+
+fn action_rename() {
+    todo!()
+}
+
+fn action_copy() {
+    todo!()
+}
+
+fn action_move() {
+    todo!()
+}
+
+fn action_open() {
+    todo!()
+}
+
+fn action_view() {
+    todo!()
+}
+
+fn action_permissions() {
+    todo!()
+}
+
+fn action_compress() {
+    todo!()
+}
+
+fn action_delete() {
+    todo!()
+}
+
+// Helpers
+
+fn is_text_file() {
+    todo!()
+}
+
+fn try_copy_to_clipboard() {
+    todo!()
+}
+
+fn shell_escape() {
+    todo!()
+}
+
--- src/menu.rs
+++ src/menu.rs
@@ -1,8 +1,32 @@
 use std::io;
-use std::path:PathBuf;
+use std::path::PathBuf;
 
 use crossterm::{
     event::{self, Event, KeyCode, KeyEventKind},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
+
+use ratatui::{
+    backend::CrosstermBackend,
+    layout::{Alignment, Constraint, Direction, Layout},
+    style::{Color, Modifier, Style},
+    text::{Line, Span},
+    widgets::{Block, BorderType, Borders, List, ListItem, ListState, Paragraph},
+    Terminal,
+};
+
+use crate::actions::{self, FileAction, GeneralAction};
+
+pub fn run_general_menu() {
+    todo!()
+}
+
+pub fn run_file_menu() {
+    todo!()
+}
+
+fn pick_item() {
+    todo!()
+}
+
